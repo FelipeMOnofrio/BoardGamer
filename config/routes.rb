15e992d55b3b13ec3660cf0e1d3edacddf270e87
Felipe <felipe@fmo4web.ca>
2018-03-21 12:15:25 -0500

    Adding a frontend to display boardgame details.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  #get 'board_games', to: 'board_games#index'
+  ##get 'board_games/:id', to: 'board_games#show'
+
+  #take the 2 gets up and put in resources
+  resources :board_games, only: [:index, :show]
+
+  root to: 'board_games#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
