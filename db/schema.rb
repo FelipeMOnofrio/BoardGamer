477b4890a0c08840d838d93cdfdc3a1b5777ecc1
Felipe <felipe@fmo4web.ca>
2018-03-25 16:05:07 -0500

    Added an image prop to board games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321160301) do
+ActiveRecord::Schema.define(version: 20180325205151) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180321160301) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["category_id"], name: "index_board_games_on_category_id"
   end
 
